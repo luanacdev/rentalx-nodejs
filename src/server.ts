2ea8b515014ba0543607ef10d762f714471d01b3
luacam20 <luana.cam20@gmail.com>
2022-10-06 16:18:39 -0300

    Add imports on documentation

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
-import "reflect-metadata";
 import express from 'express';
-
-
+import "reflect-metadata";
 import  "./database";
 import "./shared/container"
+
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
+
+
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
