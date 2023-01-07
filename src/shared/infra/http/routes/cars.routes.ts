d0556a05ce81a88a568d572cce4359754974063a
luanacdev <luana.cam20@gmail.com>
2023-01-06 22:45:13 -0300

    UseCase add car images

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,16 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
+
+import uploadConfig from "../../../../config/upload";
 
 import { CreateCarController } from "@modules/cars/UseCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/UseCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/UseCases/listAvailableCars/ListAvailableCarsController";
+import { UploadCarImageController } from "@modules/cars/UseCases/uploadCarImages/UploadCarImagesController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
+const upload = multer(uploadConfig.upload("./tmp/cars"));
+
+
 let createCarController = new CreateCarController();
 let listAvailableCarsController = new ListAvailableCarsController();
 let createCarSpecificationController = new CreateCarSpecificationController();
+let uploadCarImageController = new UploadCarImageController();
+
 
 carsRoutes.post(
     "/", 
@@ -30,5 +39,14 @@ carsRoutes.post(
     ensureAdmin,
     createCarSpecificationController.handle
 )
+
+carsRoutes.post(
+    "/images/:id", 
+    ensureAuthenticated, 
+    ensureAdmin,
+    upload.array("images"),
+    uploadCarImageController.handle
+)
+
 
 export { carsRoutes };
