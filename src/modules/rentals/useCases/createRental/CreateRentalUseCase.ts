0d2102a7f38d8159327c4ab93ca09cfaa65d482f
luanacdev <luana.cam20@gmail.com>
2023-01-12 21:27:22 -0300

    Create DateProvider

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,10 +1,7 @@
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-
-dayjs.extend(utc);
 
 interface IRequest {
     user_id: string;
@@ -15,7 +12,8 @@ interface IRequest {
 class CreateRentalUseCase {
 
     constructor(
-        private rentalsRepository: IRentalsRepository
+        private rentalsRepository: IRentalsRepository,
+        private dateProvider: IDateProvider
     ) {}
 
     async execute({
@@ -36,15 +34,10 @@ class CreateRentalUseCase {
         if (rentalOpenToUser) {
             throw new AppError("There is a rental in progress for user!")
         }
-
-        const expectedReturnDateFormat = dayjs(expected_return_date)
-        .utc()
-        .local()
-        .format();
-
-        const dateNow = dayjs().utc().local().format();
         
-        const compare = dayjs(expectedReturnDateFormat).diff(dateNow, 'hours')
+        const dateNow = this.dateProvider.dateNow();
+
+        const compare = this.dateProvider.compareInHours(dateNow, expected_return_date)
 
         if (compare < minimumHour) {
             throw new AppError("Invalid return time!");
